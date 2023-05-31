5280caed1e31d63fa32d7a39df646ab79b2b1db3
Mohammed Alomair <Alomair91@gmail.com>
2023-05-31 14:47:02 +0300

    refactor routing

--- src/main/kotlin/com/omairtech/Application.kt
+++ src/main/kotlin/com/omairtech/Application.kt
@@ -1,9 +1,7 @@
 package com.omairtech
 
+import com.omairtech.plugins.configureRouting
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import java.util.Date
 
 // https://start.ktor.io/ => to create new project
 
@@ -11,12 +9,5 @@ fun main(args: Array<String>) : Unit = io.ktor.server.netty.EngineMain.main(args
 
 
 fun Application.module(){
-    routing {
-        get("/") {
-//            call.respondText("Hello, World!")
-            call.respondText {
-                return@respondText "Hello, World! 1 ${Date().time}"
-            }
-        }
-    }
+    configureRouting()
 }
