768c8589685af0d109475b3dbbf802f284ea6694
Mohammed Alomair <Alomair91@gmail.com>
2023-05-31 17:06:40 +0300

    request assets

--- src/main/kotlin/com/omairtech/modules/routings/routes/Home.kt
+++ src/main/kotlin/com/omairtech/modules/routings/routes/Home.kt
@@ -2,6 +2,7 @@ package com.omairtech.modules.routings.routes
 
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -16,6 +17,14 @@ fun Application.configureHomeRouting() {
 //        }
 //    }
     routing {
+        // http://127.0.0.1:8080/assets/images/*.*
+        // http://127.0.0.1:8080/assets/htmlWelcome
+        // http://127.0.0.1:8080/assets/txtWelcome
+        static(remotePath = "assets") {
+            resources(resourcePackage = "images") // import all the images in the folder name images
+            resource(remotePath = "htmlWelcome", resource = "html/welcome.html")
+            resource(remotePath = "txtWelcome", resource = "txt/welcome.txt")
+        }
 
         // http://127.0.0.1:8080
         get("/") {
@@ -23,7 +32,7 @@ fun Application.configureHomeRouting() {
         }
 
         // http://127.0.0.1:8080/home
-        route(path = "/home", method = HttpMethod.Get){
+        route(path = "/home", method = HttpMethod.Get) {
             handle {
                 call.respondText("Hello World!!! ${Date().time}")
             }
@@ -33,8 +42,8 @@ fun Application.configureHomeRouting() {
         get("/home/{userName}") {
             val userName = call.parameters["userName"]
             val connection = call.request.header("Connection")
-            if(userName == "Admin"){
-                call.response.header("CustomHeader","Admin")
+            if (userName == "Admin") {
+                call.response.header("CustomHeader", "Admin")
                 call.respond(message = "Hello $userName", status = HttpStatusCode.OK)
             }
             call.respondText("Hello $userName! ${Date().time} - $connection")
@@ -48,26 +57,26 @@ fun Application.configureHomeRouting() {
         }
 
         // http://127.0.0.1:8080/person
-        get("/person"){
+        get("/person") {
             val person = Person(name = "Jon", age = 23)
             try {
                 call.respond(message = person, status = HttpStatusCode.OK)
 
-            }catch (e:Exception){
+            } catch (e: Exception) {
                 call.respond(message = e.message.toString(), status = HttpStatusCode.BadRequest)
 
             }
         }
 
         // http://127.0.0.1:8080/redirect
-        get("/redirect"){
+        get("/redirect") {
             call.respondRedirect(url = "/moved", permanent = false)
         }
-        get("/moved"){
+        get("/moved") {
             call.respondText("redirect to moved")
         }
     }
 }
 
 @Serializable
-data class Person(val name:String,val age:Int)
+data class Person(val name: String, val age: Int)
